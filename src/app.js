3f27315efeaf02354bd89c22fdd291afea61d588
Mohammed - ALKhafaji <ezio.ahmed12@gmail.com>
2026-06-14 18:51:29 +0300

    Impelment Eslint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello From DevOps Api Project!!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello From DevOps Api Project!!');
 });
 
 export default app;

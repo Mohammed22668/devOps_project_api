cde19aad33a0f490b64f08b9c5b78e7399d1283e
Mohammed - ALKhafaji <ezio.ahmed12@gmail.com>
2026-06-16 13:51:01 +0300

    Add Auth

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { signup, signIn } from '#controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/sign-up', signup);
+
+router.post('/sign-in', signIn);
+
+router.post('/sign-out', (req, res) => {
+  res.send('POST /api/auth/sign-out response');
+});
+
+export default router;
